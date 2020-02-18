ae5341382947b580a475b1b349b750bd6367e856
Christopher Tee <christopherfytee@gmail.com>
2020-02-18 15:21:24 -0500

    :sparkles:Allow dynamic resizing of progress bar

--- src/utils/multiproc.py
+++ src/utils/multiproc.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 
 def parallelize(func,
                 iterable,
+                length: Optional[int] = None,
                 n_workers: Optional[int] = None,
                 desc: Optional[str] = None,
                 chunksize: Optional[int] = None,
@@ -14,8 +15,10 @@ def parallelize(func,
                ):
     workers = (cpu_count() - 1) if n_workers is None else n_workers
     chunksize = 1 if chunksize is None else chunksize
+    total = len(iterable) if length is None else length
     func = partial(func, **func_kwargs)
 
     with Pool(workers) as p:
         return list(tqdm(p.imap(func, iterable, chunksize=chunksize),
-                         desc=desc, total=len(iterable)))
+                         desc=desc, dynamic_ncols=True,
+                         total=total))
