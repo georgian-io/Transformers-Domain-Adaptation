d230c50cc6d72db317f067cb16e218f31436ebcc
Christopher Tee <christopherfytee@gmail.com>
2020-02-21 14:50:56 -0500

    :sparkles:Include script to extract NER labels

---
+++ scripts/etl/biology/tasks/extract_ner_labels.py
@@ -0,0 +1,36 @@
+"""Script to extract NER labels."""
+import argparse
+from pathlib import Path
+
+import pandas as pd
+
+
+LABEL_FILE = 'labels.txt'
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        "Extract labels from NER data.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument('src', type=Path,
+                        help='Directory the train, dev and test NER files')
+    parser.add_argument('--suffix', type=str, default='.tsv',
+                        help='NER file types')
+    return parser.parse_args()
+
+
+def main(args):
+    df = pd.concat([
+        pd.read_csv(p, sep='\t', names=['word', 'label']) for p in args.src.rglob(f'*{args.suffix}')
+    ], ignore_index=True)
+
+    labels = df['label'].unique().tolist()
+
+    with open(args.src / LABEL_FILE, 'w+') as f:
+        f.write('\n'.join(labels))
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
