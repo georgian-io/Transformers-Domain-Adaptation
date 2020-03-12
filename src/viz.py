6b3bd41641c4cc218ca8cbc4132c88791b6907d0
Christopher Tee <christopherfytee@gmail.com>
2020-03-12 14:10:25 -0400

    ⚗️ Normalize metric by benchmark

--- src/viz.py
+++ src/viz.py
@@ -14,4 +14,12 @@ def parallel_coord_plot(df: pd.DataFrame, metric: str = "test_f1"):
     # Filter metrics that are not part the `metric`
     df = df.drop([[col for col in df.columns if 'test' in col and col != metric]], axis=1)
 
+    # Create metric that is relative to benchmark and drop benchmark
+    df = (
+        df
+        .groupby('fine_tune_dataset')
+        .apply(lambda df: df.assign(metric=df[metric] - df[(df['corpora'] == 'BERT') & (df['vocab'] == 'BERT')][metric]))
+        .pipe(lambda df: df[df['metric'] != 0])
+    )
+
     return px.parallel_coordinates(df, color=metric)
