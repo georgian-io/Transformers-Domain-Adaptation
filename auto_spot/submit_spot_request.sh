323bf9978375a192bfadbde180a699d0a9677986
Christopher Tee <christopherfytee@gmail.com>
2020-04-05 17:15:49 -0400

    ✨ Parametrize setup with git branch

--- auto_spot/submit_spot_request.sh
+++ auto_spot/submit_spot_request.sh
@@ -1,18 +1,14 @@
 #!/bin/zsh
 
-# Ensure that instance used for setup is terminated
-# (so that volume can be deleted if in different AZ as spot instance when
-# running user_data_script
-EBS_ATTACHED=$(
-    aws ec2 describe-volumes \
-    --filter "Name=tag:Name,Values=autonlp-checkpoints" \
-    --query "Volumes[0].Attachments" \
-    --output text
-)
-if ! [ -z $EBS_ATTACHED ]; then
-    echo "EBS volume may still be connected to the setup machine. \
-    Please terminate the instance or detach the volume to ensure that \
-    user_data_script.sh works."
+SPOT_FLEET_CONFIG="auto_spot/spot_fleet_config.json"
+USER_DATA_SCRIPT="auto_spot/user_data_script.sh"
+
+GIT_BRANCH=$1
+if [ -z $GIT_BRANCH ]; then
+    echo "Valid git branch expected as the first argument"
+    exit 1
+elif ! [ $(git branch --remote | grep $GIT_BRANCH | wc -l | xargs) = 1 ]; then
+    echo "Git branch provided does not exist at origin."
     exit 1
 fi
 
@@ -20,10 +16,13 @@ fi
 # 1. Update user script into spot_fleet_config.json
 # 2. Submit spot fleet request
 # 3. Reset values in folder
-USER_DATA=$(base64 auto_spot/user_data_script.sh -b0) \
-    && sed -i '' \
-        "s|base64_encoded_bash_script|$USER_DATA|g"  \
-        auto_spot/spot_fleet_config.json \
+
+sed -i '' "s/git-branch-temp/$GIT_BRANCH/g" $USER_DATA_SCRIPT
+USER_DATA_B64=$(base64 $USER_DATA_SCRIPT -b0) \
+    && sed -i '' "s|base64_encoded_bash_script|$USER_DATA_B64|g" $SPOT_FLEET_CONFIG \
     && aws ec2 request-spot-fleet \
-        --spot-fleet-request-config file://auto_spot/spot_fleet_config.json \
-    && git checkout @ -- auto_spot/spot_fleet_config.json
+        --spot-fleet-request-config file://$SPOT_FLEET_CONFIG \
+
+# Undo sed substitutions
+sed -i '' "s/$GIT_BRANCH/git-branch-temp/g" $USER_DATA_SCRIPT
+git checkout @ -- $SPOT_FLEET_CONFIG
