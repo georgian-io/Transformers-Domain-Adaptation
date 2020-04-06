f0994f8efacb039e1df481483118fdfd94398f35
Christopher Tee <christopherfytee@gmail.com>
2020-04-06 11:52:11 -0400

    📝 Create tmux training template

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SESSION='dpt'
+SESSION='autonlp'
 
 # Find or create a tmux session
 if tmux has-session -t $SESSION 2> /dev/null; then
@@ -8,14 +8,23 @@ else
     tmux new-session -s $SESSION -d
 fi
 
-# First window for general stuff
-tmux send-keys "source activate pytorch_p36" C-m
-tmux send-keys "./run_pipeline.sh" C-m
+# Training panes
+DPT_COMPLETIONS=( 10 25 50 75 )
+for DPT_COMPLETION in ${DPT_COMPLETIONS[@]}; do
+    tmux split-window -v
+    tmux send-keys "echo $DPT_COMPLETION" C-m
+    # tmux send-keys "source activate pytorch_p36" C-m
+    # tmux send-keys "./run_pipeline.sh $DPT_COMPLETION" C-m
+    tmux select-layout even-vertical
+done
+tmux kill-pane -t 0
+tmux select-layout even-vertical
 
-# Second window for htop
+
+# Second pane for htop
 tmux split-window -v
 tmux send-keys "htop" C-m
 
-# Third window for GPU usage
+# Third pane for GPU usage
 tmux split-window -h
 tmux send-keys "watch -n 1 nvidia-smi" C-m
