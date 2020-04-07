b84d4a4899e2f25b2ace167b7e60ec5963319c39
Christopher Tee <christopherfytee@gmail.com>
2020-04-07 17:18:56 -0400

    ⚗ Update configuration for DPT most diverse corpus subset

--- scripts/sync_s3_data.sh
+++ scripts/sync_s3_data.sh
@@ -7,8 +7,15 @@ if [ $(basename $(pwd)) != "NLP-Domain-Adaptation" ]; then
     exit 1
 fi
 
+# Copy cached folders
+if ! [ -e results ]; then mkdir results; fi
+
+FINE_TUNE_DATASET="linnaeus"
+PCT=2
+MOD="most"
+
 # Copy corpus and fine-tuning datasets from S3
-DOMAINS=("biology" "law")
+DOMAINS=("biology")
 SUBDIRECTORIES=("corpus" "tasks")
 for domain in $DOMAINS; do
     # # Load corpus
@@ -23,16 +30,18 @@ for domain in $DOMAINS; do
     fi
 done
 
-# Copy cached folders
-if ! [ -e results ]; then mkdir results; fi
+DATA_DIR="data/biology/corpus/subsets/"
+RESULTS_DIR="results/$FINE_TUNE_DATASET/pubmed_${PCT}pct_${MOD}_diverse/domain-pre-trained"
 
-DPT_COMPLETIONS=( [10]=10000 [25]=30000 [50]=60000 [75]=95000 )
-for DPT_COMPLETION in ${!DPT_COMPLETIONS[@]}; do
-    CKPT_NUM=${DPT_COMPLETIONS[$DPT_COMPLETION]}
-    DEST=results/linnaeus/pubmed_2pct_seed281_${DPT_COMPLETION}pct_dpt/domain-pre-trained
-    mkdir -p $DEST
-    aws s3 cp "$BUCKET/cache/pubmed_2pct_seed281/domain-pre-trained/checkpoint-$CKPT_NUM" $DEST --recursive --exclude "*pt"
-done
+mkdir -p $DATA_DIR
+mkdir -p $RESULTS_DIR
+
+aws s3 cp "$BUCKET/domains/biology/corpus/subsets/pubmed_corpus_${MOD}_diverse_entropy_0.02pct.txt" \
+    "data/biology/corpus/subsets/pubmed_corpus_${MOD}_diverse_entropy_0.02pct.txt"
+
+aws s3 cp "$BUCKET/runs/$FINE_TUNE_DATASET/pubmed_${PCT}pct_${MOD}_diverse" \
+    "results/$FINE_TUNE_DATASET/pubmed_${PCT}pct_${MOD}_diverse" \
+    --recursive
 
 # Update write permissions so other scripts can write into these folders
 sudo chmod -R 777 data
