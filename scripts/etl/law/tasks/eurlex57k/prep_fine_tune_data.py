3533efca3b691776e7fe73c2db8244a1f696a20f
Christopher Tee <christopherfytee@gmail.com>
2020-05-09 17:51:05 -0400

    ✨ Write labels to disk
    Labels have been removed of zero-shot labels

--- scripts/etl/law/tasks/eurlex57k/prep_fine_tune_data.py
+++ scripts/etl/law/tasks/eurlex57k/prep_fine_tune_data.py
@@ -1,6 +1,7 @@
 """Script to generate training, dev, and test sets for Eurlex57k dataset."""
 import logging
 import argparse
+import itertools as it
 from pathlib import Path
 from typing import Optional
 
@@ -33,15 +34,37 @@ def main(args: argparse.Namespace) -> None:
     df = pd.read_parquet(args.eurlex_src)
     df['main_body'] = df['main_body'].apply(lambda x: '\n'.join(x))
 
+    # Write text data for each dataset
     for mode in ('train', 'dev', 'test'):
-        logging.info(f'Generating texts for {mode} set')
+        logger.info(f'Generating texts for {mode} set')
         subset = df[df['dataset'] == mode]
         text_cols = subset[Eurlex57kDataset.TEXT_COLS]
         texts = Eurlex57kDataset.preprocess_texts(text_cols)
 
-        output_text_file = (args.dst / mode).with_suffix('.txt')
+        output_text_file = args.dst / f'{mode}.txt'
         output_text_file.write_text('\n'.join(texts), encoding='utf-8')
-        logging.info(f'Saved text for {mode} set to {output_text_file}')
+        logger.info(f'Saved text for {mode} set to {output_text_file}')
+
+    # Remove zero-shot labels from "concepts" field
+    logger.info('Removing zero-shot labels')
+    concept_sets = (
+        df.groupby('dataset')['concepts']
+        .apply(lambda series: set(it.chain.from_iterable(series.tolist())))
+    )
+    dev, test, train = concept_sets
+    zero_shot_labels = (dev - train) | (test - train)
+    df['concepts'] = df['concepts'].apply(lambda concepts: set(concepts) - zero_shot_labels)
+
+    for mode in ('train', 'dev', 'test'):
+        logger.info(f'Generating labels for {mode} set')
+        labels_subset = (
+            df[df['dataset'] == mode]['concepts']
+            .apply(lambda x: ' '.join(x))
+        )
+
+        output_label_file = args.dst / f'{mode}_labels.txt'
+        output_label_file.write_text('\n'.join(labels_subset.tolist()))
+        logger.info(f'Saved labels for {mode} set to {output_label_file}')
 
 
 if __name__ == '__main__':
