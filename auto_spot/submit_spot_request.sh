f2a9144dee53b5a1da6d2fdb0302a05c4db1f441
Christopher Tee <christopherfytee@gmail.com>
2020-05-12 10:25:37 -0400

    ⚗ [EXP] Vocab aug. PubMed 10% (Random seed 42)

--- auto_spot/submit_spot_request.sh
+++ auto_spot/submit_spot_request.sh
@@ -1,9 +1,18 @@
 #!/bin/zsh
 
+MODE=$1
+VALID_MODES=("dpt" "ft")
+if [ -z $MODE ]; then echo "Mode required as first arg."; exit 1; fi
+if ! [ $MODE = "dpt" ] && ! [ $MODE = "ft" ]; then
+    echo "Invalid `mode` provided."
+    exit 1
+fi
+
+INSTANCE_TYPE=$([ $MODE = "dpt" ] && echo "p3.16xlarge" || echo "p3.2xlarge")
 SPOT_FLEET_CONFIG="auto_spot/spot_fleet_config.json"
 USER_DATA_SCRIPT="auto_spot/user_data_script.sh"
 
-GIT_BRANCH=$1
+GIT_BRANCH=$2
 if [ -z $GIT_BRANCH ]; then GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD); fi
 
 # Check if remote git branch exists to run code on VM
@@ -25,6 +34,7 @@ fi
 sed -i '' "s/git-branch-temp/$GIT_BRANCH/g" $USER_DATA_SCRIPT
 USER_DATA_B64=$(base64 $USER_DATA_SCRIPT -b0) \
     && sed -i '' "s|base64_encoded_bash_script|$USER_DATA_B64|g" $SPOT_FLEET_CONFIG \
+    && sed -i '' "s/instance_type/$INSTANCE_TYPE/g" $SPOT_FLEET_CONFIG \
     && aws ec2 request-spot-fleet \
         --spot-fleet-request-config file://$SPOT_FLEET_CONFIG \
 
